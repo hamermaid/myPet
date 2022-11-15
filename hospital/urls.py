1fe5c4473f822fb05a0a6a8d9d388ac50442ebce
JHaeun <lydia_he@naver.com>
2022-11-15 18:21:55 +0900

    병원 상세 정보(HOS-0003) 완료

--- hospital/urls.py
+++ hospital/urls.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.urls import path
 
-from hospital.views import list
+from hospital.views import info
 
 app_name = 'hospital'
 
 urlpatterns = [
-    path('list/', list, name='list'),
+    path('info/<str:hos_id>', info, name='info'),
 ]
--- hospital/views.py
+++ hospital/views.py
@@ -4,32 +4,44 @@ from django.shortcuts import render
 # Create your views here.
 from hospital.models import Hospital
 from modual.response import DefaultResponse
+from reviews.models import Reviews
 
 
-def list(request):
+def info(request, hos_id):
     if request.method == "GET":
-        req = request.GET['id']
-        hos = Hospital.objects.get(hos_id=req)
+        try:
+            hos = Hospital.objects.get(hos_id=hos_id)
+        except Hospital.DoesNotExist:
+            return DefaultResponse(400, '잘못된 데이터입니다.')
+        review = Reviews.objects.count()    # 해당 병원 리뷰 카운트 가져와야함 - 수정필요
+        # hos = Hospital.objects.get(hos_id=req)
         # data = list(hos.values())
-        print('hos', hos)
-        keyword = hos.animal_keyword.split(',') + hos.service_keyword.split(',')
-        # 토큰에서 userId 찾기
-        user = 'Test00'
-        # 유저의 즐겨찾기 여부
-        favorite = hos.is_favorite.split(',')
-        is_favorite = False
-        for i in favorite:
-            if i == user:
-                is_favorite = True
+        animal = hos.animal.split(',')
+        info = hos.facilitly.split(',')
+        open = hos.open_time.split('/')
+        restAt = []
+        openAt = []
+        for i in open:
+            if '휴무' in i:
+                restAt.append(i.replace(' 휴무', '').strip().split(','))
+                # restAt = i
+            else:
+                openAt.append(i.strip())
+        # # 토큰에서 userId 찾기
+        # user = 'Test00'
         data = {
-            "hospital_name": hos.name,
-            "address": hos.address,
-            # "ratings": hos.ratings,
-            # "reviews": hos.reviews,
-            # "open_hour": hos.open_hour,
-            "restAt": hos.restAt,
-            "labels": [keyword],
-            "is_favorite": is_favorite,
+            "hospital_name": hos.name,      # 병원이름
+            "number": hos.number,           # 전화번호
+            "page": hos.link,               # 홈페이지
+            "address": hos.address,         # 주소
+            "subway": hos.subway,           # 지하철역
+            "reviews": review,              # 리뷰수
+            "open_hour": openAt,            # 오픈 시간
+            "restAt": restAt,               # 휴무일
+            "lunch_hour": hos.break_time,   # 점심시간
+            "ratings": hos.star,            # 별점
+            "care_available": animal,       # 케어 동물
+            "facilities": info,             # 시설정보
           }
-    # return DefaultResponse(200, testData)
+        print('animal', data)
     return DefaultResponse(200, data)
